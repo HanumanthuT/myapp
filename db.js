9ae6b7e65a48f3fa68530c6c4b14288a1c2e24e6
TalariHanumanthu <talari2659@gmail.com>
2026-02-20 13:55:10 +0530

    updated db

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "testdb.c1oiemy84zfq.ap-south-1.rds.amazonaws.com",
+  host: "testdb01.c1oiemy84zfq.ap-south-1.rds.amazonaws.com",
   user: "admin",
   password: "Sanju_11233",  // Replace with your MySQL password
-  database: "testdb"
+  database: "testdb01"
 });
 
 connection.connect((err) => {
